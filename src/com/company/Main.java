f33fef58c2a3e8bb128e48da5718506ac04b95b3
Leonel Perez <leonelperezrios@gmail.com>
2022-04-10 18:10:06 -0500

    Llamado de funcion y comunicacion entre clases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-/*Primera parte:
+    /*Primera parte:
 
             Crear una función con tres parámetros que sean números que se suman entre sí.
             Llamar a la función en el main y darle valores.
